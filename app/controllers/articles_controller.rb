d86bdce2a2609af75db162080a6d9d1c1e5e1b8d
Alexander Kurakov <zmshurik@yandex.ru>
2018-08-16 21:47:19 +0700

    add article#edit action

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -11,8 +11,12 @@ class ArticlesController < ApplicationController
         @article = Article.new
     end
 
+    def edit
+        @article = Article.find params[:id]
+    end
+
     def create
-        @article = Article.new(params.require(:article).permit(:title, :text))
+        @article = Article.new article_params
 
         if @article.save
             redirect_to @article
@@ -20,4 +24,18 @@ class ArticlesController < ApplicationController
             render 'new'
         end
     end
+
+    def update
+        @article = Article.find params[:id]
+
+        if Article.update article_params
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+    private
+        def article_params
+            params.require(:article).permit :title, :text
+        end
 end
