77157882f002c612ebd639ccb0354fc784795324
Alexander Kurakov <zmshurik@yandex.ru>
2018-08-31 19:29:34 +0700

    fix indentation

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    has_many :comments
-    validates :title, presence: true, length: { minimum: 5 }
+  has_many :comments
+  validates :title, presence: true, length: { minimum: 5 }
 end
